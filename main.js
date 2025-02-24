b6d6b3a56301554a21251ab602551d8ce6113cdb
Minjung Kim <fgh2018@naver.com>
2025-02-25 02:25:26 +0900

    implementation of WAS

--- main.js
+++ main.js
@@ -6,32 +6,60 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;
 
-    if (pathname == '/'){
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+    if (pathname === '/'){
+        if(queryData.id === undefined){
+            // Welcome 페이지
+            var title = "Welcome";
+            var description = "Hello, Node.js";
             var template = `
             <!doctype html>
             <html>
             <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
             </head>
             <body>
-            <h1><a href="/?id=home">WEB</a></h1>
-            <ol>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
-            <h2>${title}</h2>
-            <p>${description}</p>
+                <h1><a href="/">WEB</a></h1>
+                <ol>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                </ol>
+                <h2>${title}</h2>
+                <p>${description}</p>
             </body>
             </html>
             `;
             response.writeHead(200);
-            response.end(template); // 사용자에게 전송하는 데이터가 바뀌는게 해주는 문 
-        });
+            response.end(template);
+        } else {
+            // ID가 있는 경우
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+                var title = queryData.id;
+                var template = `
+                <!doctype html>
+                <html>
+                <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ol>
+                        <li><a href="/?id=HTML">HTML</a></li>
+                        <li><a href="/?id=CSS">CSS</a></li>
+                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ol>
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
     } else {
         response.writeHead(404);
         response.end("Not found");
